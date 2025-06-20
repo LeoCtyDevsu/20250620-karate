b8dd50b0bf37c6af8f5ca8df2d0f54b90fd665a7
Leandro Carmenaty <lcarmena@pichincha.com>
2025-06-20 11:06:38 -0500

    feat: Implementar pruebas automatizadas para la API de personajes Marvel con Karate

--- src/test/java/com/pichincha/TestRunner.java
+++ src/test/java/com/pichincha/TestRunner.java
@@ -2,30 +2,16 @@ package com.pichincha;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestRunner {
     
     @Test
-    void testParallel() {
+    void testMarvelCharacters() {
         Results results = Runner.path("classpath:com/pichincha/features")
-                .tags("~@ignore")
+                .outputCucumberJson(true)
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
-    }
-
-    @Karate.Test
-    Karate testTags() {
-        return Karate.run()
-                .tags("@E2", "@Agente2")
-                .relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate testMarvelCharacters() {
-        return Karate.run().relativeTo(getClass()).path("com/pichincha/features");
     }
 }
